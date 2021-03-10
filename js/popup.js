c46b8c2df71ffb96df4463f8b1064ee4ddc99202
Oksana-KV51 <12345-70@mail.ru>
2021-03-10 17:20:18 +0400

    Оживляем интерактивные элементы

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+const btncontacts = document.querySelector(".btn-contacts");
+const popup = document.querySelector(".appoiment");
+const close = document.querySelector(".appoiment-close");
+
+btncontacts.addEventListener("click", function (evt) {
+    evt.preventDefault();       
+    popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();       
+    popup.classList.remove("modal-show");
+}); 
