36c55f4c5e865443d9bbd733a0248f80fa9300f9
Oksana-KV51 <12345-70@mail.ru>
2021-03-11 12:03:35 +0400

    Оживляем интерактивные элементы. 2 работа над ошибками

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,10 @@
 const btncontacts = document.querySelector(".btn-contacts");
 const popup = document.querySelector(".appoiment");
 const close = document.querySelector(".appoiment-close");
-
 btncontacts.addEventListener("click", function (evt) {
     evt.preventDefault();       
     popup.classList.add("modal-show");
 });
-
 close.addEventListener("click", function (evt) {
     evt.preventDefault();       
     popup.classList.remove("modal-show");
